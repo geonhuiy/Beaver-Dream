267a918d95ce4b5861a34d6643b48c40ee36bfb0
Jamie <rkawk3316@gmail.com>
2020-02-02 01:24:51 +0200

    Restructured item pickup

--- Assets/Scripts/BeaverItemPickup.cs
+++ Assets/Scripts/BeaverItemPickup.cs
@@ -6,22 +6,20 @@ public class BeaverItemPickup : MonoBehaviour
 {
     private bool hasObject = false;
     private GameObject currentHeldObj;
-    /// <summary>
-    /// OnCollisionEnter is called when this collider/rigidbody has begun
-    /// touching another rigidbody/collider.
-    /// </summary>
-    /// <param name="other">The Collision data associated with this collision.</param>
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
         hasObject = true;
         currentHeldObj = other.gameObject;
+
+        //objTransform.transform.parent = transform;
+        //objTransform.transform.position = holdSlot.transform.position;
     }
 
     private void Update()
     {
         if (hasObject)
         {
-            currentHeldObj.transform.position = this.transform.position;
+            currentHeldObj.transform.position = this.gameObject.transform.GetChild(0).transform.position;
             Vector3 rotation = new Vector3(currentHeldObj.transform.eulerAngles.x, this.transform.eulerAngles.y, currentHeldObj.transform.eulerAngles.z);
             currentHeldObj.transform.rotation = Quaternion.Euler(rotation);
         }
