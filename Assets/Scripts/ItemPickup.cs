4cb225acdc0ef41baa8e45f9abdda9528e2c2875
Jamie <rkawk3316@gmail.com>
2020-02-01 16:29:35 +0200

    Added mouse click to move for debugging

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    [SerializeField]
+    private bool hasObject = false;
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+}
