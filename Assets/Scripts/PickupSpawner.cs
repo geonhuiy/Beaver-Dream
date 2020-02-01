2a12aceb91532fe9fd7573527ea3d41b3a4cc16c
Jamie <rkawk3316@gmail.com>
2020-02-02 01:53:23 +0200

    Added rock prefabs

---
+++ Assets/Scripts/PickupSpawner.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickupPool;
+}
