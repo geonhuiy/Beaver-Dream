95a836541702efcd7a6e19808d804bc671f87d35
Jamie <rkawk3316@gmail.com>
2020-02-01 19:08:01 +0200

    Added beaver item pickup

---
+++ Assets/Scripts/BeaverItemPickup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaverItemPickup : MonoBehaviour
+{
+    private bool hasObject = false;
+    private GameObject currentHeldObj;
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        hasObject = true;
+        currentHeldObj = other.gameObject;
+    }
+
+    private void Update()
+    {
+        if (hasObject)
+        {
+            currentHeldObj.transform.position = this.transform.position;
+        }
+    }
+}
--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -10,13 +10,21 @@ public class ItemPickup : MonoBehaviour
     /// <param name="other">The other Collider involved in this collision.</param>
     [SerializeField]
     private bool hasObject = false;
+    private Collider currentCollider;
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Beaver");
+        // Debug.Log("Beaver");
+        currentCollider = other;
+        hasObject = true;
+        // this.gameObject = other.gameObject;
     }
 
     private void Update()
     {
-
+        if (hasObject)
+        {
+            //this.gameObject.transform.localScale = new Vector3(5,5,5);
+            this.gameObject.transform.position = currentCollider.gameObject.transform.position;
+        }
     }
 }
